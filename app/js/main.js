0bfbdd094eaca3c44a9e4267208c423d7f86266c
LiRein <sergey11200011@mail.ru>
2022-01-21 20:00:30 +0500

    added products, video, blog, partners, design

--- app/js/main.js
+++ app/js/main.js
@@ -5,7 +5,16 @@ $(function(){
     dots: true,
     arrows: false,
   });
+ 
+  var products = document.querySelector('[data-ref="products"]');
+  var design = document.querySelector('[data-ref="design"]');
 
-  var mixer = mixitup('.products__items');
-  
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+
+  var mixer1 = mixitup('.products', config);
+  var mixer2 = mixitup('.design', config);
 });
